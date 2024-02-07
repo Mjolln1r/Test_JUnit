d59395f50658b8d939092fbea06f57a9cef3a42b
Nice <kponomarenko077@gmail.com>
2024-02-07 22:33:04 +0300

    Initial commit

---
+++ src/main/java/ru/itis/math/Main.java
@@ -0,0 +1,9 @@
+package ru.itis.math;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        NumbersUtil numbersUtil = new NumbersUtil();
+        System.out.println(numbersUtil.gcd(18, 22));
+    }
+}
